50469f5b29282aa0b4b890d41c2ec5f188665b89
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2026-04-20 14:09:25 +0200

    logs

--- src/log/LoggerUtility.java
+++ src/log/LoggerUtility.java
@@ -5,19 +5,14 @@ import org.apache.log4j.PropertyConfigurator;
 
 
 public class LoggerUtility {
-	private static final String TEXT_LOG_CONFIG = "src/log/log4j-text.properties";
-	private static final String HTML_LOG_CONFIG = "src/log/log4j-html.properties";
+    private static boolean isConfigured = false;
 
-	public static Logger getLogger(Class<?> logClass, String logFileType) {
-		if (logFileType.equals("text")) {
-			PropertyConfigurator.configure(TEXT_LOG_CONFIG);
-		} else if (logFileType.equals("html")) {
-			PropertyConfigurator.configure(HTML_LOG_CONFIG);
-		} else {
-			throw new IllegalArgumentException("Unknown log file type !");
-		}
-
-		String className = logClass.getName();
-		return Logger.getLogger(className);
-	}
+    public static Logger getLogger(Class<?> logClass) {
+        if (!isConfigured) {
+            // Configuration Text et HTML
+            PropertyConfigurator.configure("src/log/log4j.properties");
+            isConfigured = true;
+        }
+        return Logger.getLogger(logClass.getName());
+    }
 }
