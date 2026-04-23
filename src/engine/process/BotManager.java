d219d6edc94cfc0adb7071aea99d84da6f25fd1b
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2026-04-23 06:27:51 +0400

    refactor: convert switch to if-else in BotManager

--- src/engine/process/BotManager.java
+++ src/engine/process/BotManager.java
@@ -82,8 +82,7 @@ public void init(List<Hero> allHeroes, Hero playerHero) {
     private List<double[]> getBotWaypoints(String botName) {
         int T = GameConfiguration.TILE_SIZE;
         List<double[]> wp = new ArrayList<>();
-        switch (botName) {
-        case "Bot1":
+        if (botName.equals("Bot1")) {
         	wp.add(new double[]{8 * T, 52 * T});
             wp.add(new double[]{6 * T, 44 * T});
             wp.add(new double[]{6 * T, 29 * T});
@@ -91,31 +90,27 @@ public void init(List<Hero> allHeroes, Hero playerHero) {
             wp.add(new double[]{21 * T, 6 * T});
             wp.add(new double[]{36 * T, 6 * T});
             wp.add(new double[]{52 * T, 6 * T});
-            break;
-        case "Bot2":
+        } else if (botName.equals("Bot2")) {
         	wp.add(new double[]{9 * T, 52 * T});
             wp.add(new double[]{16 * T, 44 * T});
             wp.add(new double[]{30 * T, 30 * T});
             wp.add(new double[]{44 * T, 16 * T});
             wp.add(new double[]{52 * T, 8 * T});
-            break;
-        case "Bot3":
+        } else if (botName.equals("Bot3")) {
         	wp.add(new double[]{8 * T, 54 * T});
             wp.add(new double[]{21 * T, 54 * T});
             wp.add(new double[]{36 * T, 54 * T});
             wp.add(new double[]{54 * T, 54 * T});
             wp.add(new double[]{54 * T, 44 * T});
             wp.add(new double[]{54 * T, 8 * T});
-            break;
-        case "Bot4":
+        } else if (botName.equals("Bot4")) {
         	wp.add(new double[]{16 * T, 44 * T});
             wp.add(new double[]{21 * T, 55 * T});
             wp.add(new double[]{36 * T, 55 * T});
             wp.add(new double[]{54 * T, 55 * T});
             wp.add(new double[]{54 * T, 44 * T});
             wp.add(new double[]{54 * T, 8 * T});
-            break;
-        case "ENEMY_Bot1":
+        } else if (botName.equals("ENEMY_Bot1")) {
         	wp.add(new double[]{52 * T, 6 * T});
             wp.add(new double[]{7 * T, 55 * T});
             wp.add(new double[]{29 * T, 6 * T});
@@ -123,31 +118,27 @@ public void init(List<Hero> allHeroes, Hero playerHero) {
             wp.add(new double[]{6 * T, 21 * T});
             wp.add(new double[]{6 * T, 44 * T});
             wp.add(new double[]{6 * T, 52 * T});
-            break;
-        case "ENEMY_Bot2":
+        } else if (botName.equals("ENEMY_Bot2")) {
         	wp.add(new double[]{52 * T, 9 * T});
             wp.add(new double[]{44 * T, 16 * T});
             wp.add(new double[]{30 * T, 30 * T});
             wp.add(new double[]{16 * T, 44 * T});
             wp.add(new double[]{8 * T, 52 * T});
-            break;
-        case "ENEMY_Bot3":
+        } else if (botName.equals("ENEMY_Bot3")) {
         	wp.add(new double[]{54 * T, 8 * T});
             wp.add(new double[]{54 * T, 21 * T});
             wp.add(new double[]{54 * T, 36 * T});
             wp.add(new double[]{54 * T, 54 * T});
             wp.add(new double[]{44 * T, 54 * T});
             wp.add(new double[]{8 * T, 54 * T});
-            break;
-        case "ENEMY_Bot4":
+        } else if (botName.equals("ENEMY_Bot4")) {
         	wp.add(new double[]{55 * T, 7 * T});
             wp.add(new double[]{55 * T, 21 * T});
             wp.add(new double[]{55 * T, 36 * T});
             wp.add(new double[]{55 * T, 54 * T});
             wp.add(new double[]{44 * T, 55 * T});
             wp.add(new double[]{8 * T, 55 * T});
-            break;
-        case "ENEMY_Bot5":
+        } else if (botName.equals("ENEMY_Bot5")) {
         	wp.add(new double[]{52 * T, 7 * T});
             wp.add(new double[]{44 * T, 7 * T});	
             wp.add(new double[]{29 * T, 7 * T});
@@ -155,7 +146,6 @@ public void init(List<Hero> allHeroes, Hero playerHero) {
             wp.add(new double[]{7 * T, 21 * T});
             wp.add(new double[]{7 * T, 44 * T});
             wp.add(new double[]{7 * T, 52 * T});
-            break;
         }
         return wp;
     }
