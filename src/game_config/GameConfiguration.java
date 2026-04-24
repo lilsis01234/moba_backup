6b45ee0ac9e30df484cdfa9e1e668b82524dc276
MiantsaFanirina <miantsafanirinarakotondrafara@gmail.com>
2026-04-24 13:43:03 +0400

    Reduce gold rewards: starting gold 0, minion 10, hero 20, tower 30

--- src/game_config/GameConfiguration.java
+++ src/game_config/GameConfiguration.java
@@ -15,10 +15,10 @@ public class GameConfiguration {
 
     
     //Gold reward    
-    public static final int GOLD_MINION = 20;
-    public static final int GOLD_CHAR    = 100;
-    public static final int GOLD_TOWER  = 150;
-    public static final int STARTING_GOLD = 500;
+    public static final int GOLD_MINION = 10;
+    public static final int GOLD_CHAR    = 20;
+    public static final int GOLD_TOWER  = 30;
+    public static final int STARTING_GOLD = 0;
     public static final int PASSIVE_GOLD_PER_SECOND = 1;
     
     //XP reward
